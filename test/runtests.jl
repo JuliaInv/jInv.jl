4aa15199ab6f7dffad29b765ddce82e52958b6b7
Dave Marchant <dwfmarchant@gmail.com>
2017-06-21 15:33:48 -0700

    Fix type-o.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ include("setupTests.jl")
 @testset "jInv" begin
 	include("Mesh/runtests.jl")
 	include("ForwardShare/runtests.jl")
-	include("Utils/runtests.jl")
+	include("Utils/runTests.jl")
 	include("InverseSolve/runtests.jl")
 	include("LinearSolvers/runtests.jl")
 end
