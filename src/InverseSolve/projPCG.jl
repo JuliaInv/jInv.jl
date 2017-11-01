0cde7f5209bcaf27ffc7c85e7f23345243abd267
Christoph Schwarzbach <christoph@compgeoinc.com>
2017-11-01 11:50:06 -0700

    Add keyword option to projPCG for verbose output

--- src/InverseSolve/projPCG.jl
+++ src/InverseSolve/projPCG.jl
@@ -9,17 +9,23 @@ export projPCG
 
 	Input:
 
-		H::Function       -  computes action of Hessian
-		g::Vector         -  right hand side
-		Active            -  describes active cells
+		H::Function       - computes action of Hessian
+		g::Vector         - right hand side
+		Active            - describes active cells
 		Precond::Function - preconditioner
 		cgTol             - tolerance
-		maxIter             - maximum number of iterations
+		maxIter           - maximum number of iterations
+    out               - verbosity level
 """
-function projPCG(H::Function,gc::Vector,Active::BitArray,Precond::Function,cgTol::Real,maxIter::Int)
+function projPCG(H::Function,gc::Vector,Active::BitArray,Precond::Function,cgTol::Real,maxIter::Int;out::Int=0)
 #  PCG over active cells
     his        = zeros(maxIter,3)
-	if norm(gc)==0; return zeros(eltype(gc),length(gc)),his[1,:]; end
+	if norm(gc)==0
+    if out >= 2
+      println("projPCG finished without iterating: trivial right hand side and solution")
+    end
+    return zeros(eltype(gc),length(gc)),his[1,:]
+  end
 
 	delm       = zeros(size(gc))
 	cgiter     = 0
@@ -53,15 +59,25 @@ function projPCG(H::Function,gc::Vector,Active::BitArray,Precond::Function,cgTol
 		rdlast = rd
 
 	    his[cgiter,1] = norm(resid)/normResid0;
+    if out >=3
+      println(@sprintf("projPCG iteration %d: relres = %.3e", cgiter, his[cgiter,1]))
+    end
 		if (his[cgiter,1] <= cgTol) || (cgiter == maxIter )
 	  	  break
 		end
 	end
+  if out >= 2
+    if his[cgiter,1] <= cgTol
+      println(@sprintf("projPCG converged at iteration %d with relres = %.3e", cgiter, his[cgiter,1]))
+    else
+      println(@sprintf("projPCG stopped at iteration %d with relres = %.3e", cgiter, his[cgiter,1]))
+    end
+  end
 
 	return delm,his[1:cgiter,:]
 end
 
-function projPCG(gc::Vector,pMis,pInv,sig::Vector,dsig,d2F,d2R,Active)
+function projPCG(gc::Vector,pMis,pInv,sig::Vector,dsig,d2F,d2R,Active;out::Int=0)
 
 		#  Set up Hessian and preconditioner.
 		Hs(x) = dsig'*HessMatVec(dsig*x,pMis,sig,d2F) + d2R*x;
@@ -69,5 +85,5 @@ function projPCG(gc::Vector,pMis,pInv,sig::Vector,dsig,d2F,d2R,Active)
 		pInv.HesPrec.setupPrec(Hs, d2R,pInv.HesPrec.param);
 		PC(x) = pInv.HesPrec.applyPrec(Hs,d2R,x,pInv.HesPrec.param);
 		# call projPCG and return
-		return projPCG(Hs,gc,Active,PC,pInv.pcgTol,pInv.pcgMaxIter)
+		return projPCG(Hs,gc,Active,PC,pInv.pcgTol,pInv.pcgMaxIter,out=out)
 end
