599baf5c1dc180815418bec8f4755814548eedf3
Lars Ruthotto <lruthotto@emory.edu>
2019-06-24 23:05:10 -0600

    cleaned up Project.toml

--- src/Utils/Utils.jl
+++ src/Utils/Utils.jl
@@ -12,19 +12,19 @@ using LinearAlgebra
 	include("uniqueidx.jl")
 	include("initRemoteChannel.jl")
 	include("variousUtils.jl")
-	
+
 
 
   import Distributed.clear!
 	export clear!
 
-	function Base.sub2ind(n::Array{Int64,1},ii::Array{Int64,1},jj::Array{Int64,1},kk::Array{Int64,1})
-		return Base.sub2ind((n[1],n[2],n[3]),ii,jj,kk)
-	end
-
-	function Base.sub2ind(n::Array{Int64,1},ii::Int64,jj::Int64,kk::Int64)
-		return Base.sub2ind((n[1],n[2],n[3]),ii,jj,kk)
-	end
+	# function Base.sub2ind(n::Array{Int64,1},ii::Array{Int64,1},jj::Array{Int64,1},kk::Array{Int64,1})
+	# 	return Base.sub2ind((n[1],n[2],n[3]),ii,jj,kk)
+	# end
+	#
+	# function Base.sub2ind(n::Array{Int64,1},ii::Int64,jj::Int64,kk::Int64)
+	# 	return Base.sub2ind((n[1],n[2],n[3]),ii,jj,kk)
+	# end
 
 	function clear!(R::RemoteChannel)
 		p = take!(R)
